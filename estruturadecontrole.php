b644fae807bed7567b85b0f63c020dc24a6362f1
Michel Geocaf <mgeocaf.developer@gmail.com>
2025-02-17 00:15:29 -0300

    exercicio sobre switch

--- estruturadecontrole.php
+++ estruturadecontrole.php
@@ -1,5 +1,5 @@
 <?php
-
+// if
 $pontosnegativos = 4;
 
 if($pontosnegativos > 3){
@@ -9,4 +9,33 @@ if($pontosnegativos > 3){
 }else{
     echo "Aluno Aprovado!";
 }
+
+
+
+
+
 ?>
+
+<!-- switch -->
+<?php
+$formpagamento = "parcelado";
+
+switch($formpagamento){
+    case 'pix':
+        echo "Pagamento com pix!";
+        break;
+
+        case 'dinheiro':
+            echo "Pagamento em dinheiro!";
+            break;
+
+            case ' debito':
+                echo "Pagamento com cartão Débito!";
+                break;
+            case ' credito':
+                echo "Pagamento com cartão Crédito!";
+                break;
+
+                default:
+                echo " Forma de pagamento inválida!";
+}
